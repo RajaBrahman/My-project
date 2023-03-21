134d3acb422c7106a3e6db5f798bf16f87c43893
RajaBrahman <120624630+RajaBrahman@users.noreply.github.com>
2023-03-21 14:06:05 +0530

    Delete Myproject/Myproject directory

--- Myproject/Myproject/src/main/java/com/springboot/project/Myproject/repository/studentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.project.Myproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.project.Myproject.model.StudentReg;
-
-public interface studentRepository extends JpaRepository<StudentReg,Long> {
-
-}
